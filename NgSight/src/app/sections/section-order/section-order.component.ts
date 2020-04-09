5b15dd436e662edfd0fc728ae7a0be2122cdb9b1
P. Baranbás <barni.pbs@gmail.com>
2020-04-09 13:57:08 +0200

    hard coded orders

--- NgSight/src/app/sections/section-order/section-order.component.ts
+++ NgSight/src/app/sections/section-order/section-order.component.ts
@@ -1,15 +1,81 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/shared/order';
 
 @Component({
   selector: 'app-section-order',
   templateUrl: './section-order.component.html',
-  styleUrls: ['./section-order.component.scss']
+  styleUrls: ['./section-order.component.scss'],
 })
 export class SectionOrderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  orders: Order[] = [
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed',
+    },
+    {
+      id: 2,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed',
+    },
+    {
+      id: 3,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed',
+    },
+    {
+      id: 4,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed',
+    },
+    {
+      id: 5,
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'Completed',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
